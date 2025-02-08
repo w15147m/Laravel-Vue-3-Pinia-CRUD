2954d51b2a881926141f174faca57dde66965c19
waseem abbas <waseemofficee@gmail.com>
2025-02-08 19:35:38 +0500

    get and addNew post

--- resources/js/store/post/postStor.js
+++ resources/js/store/post/postStor.js
@@ -1,13 +1,41 @@
 import { defineStore } from "pinia";
+import axios from "axios";
 
-export const usePostStore = defineStore('post', {
+export const postStore = defineStore("post", {
     state: () => ({
-        
+        posts: [],
+        title: "",
+        description: "",
     }),
-    getters: {
-
-    },
+    getters: {},
     actions: {
-        
-    }
-}) 
+        getPosts() {
+            axios
+                .get("/posts")
+                .then((response) => {
+                    this.posts = response.data;
+                    console.log(response.data);
+                    
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        addItem() {
+            if (this.title && this.description) {
+                let form_data = {
+                    title: this.title,
+                    description: this.description,
+                };
+                axios
+                    .post("/posts", form_data)
+                    .then((response) => {
+                        console.log(response.data);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
+        },
+    },
+});
